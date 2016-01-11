321cc305e6cb374c1eecadc49c88725142c6857a
axs5498 <axs5498@rit.edu>
2016-01-10 20:08:20 -0500

    Paper 1 codes

--- FilterandStats.R
+++ FilterandStats.R
@@ -1,5 +1,6 @@
-#The codes writted to generate the answers necessary for paper 1. 
+#The codes written to generate the answers necessary for paper 1. 
 ##The objective of this code include: 
 ##Finding the min, mean, median, sd, max, participation rate for each "dataset" generated.
 ##Further another dataset that will contains the linear regression values will also be stored. 
 ##The dataset is assumed to be a live dataset. 
+
